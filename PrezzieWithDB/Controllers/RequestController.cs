82bb03d1a3b6aafbac7d334117a810d9171ea71a
Benedikt Strozynski <strozynskib@yahoo.de>
2020-04-05 17:16:30 +0200

    Added Create Request sucessfully

---
+++ PrezzieWithDB/Controllers/RequestController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PrezzieWithDB.DAL;
+using PrezzieWithDB.Models;
+using PrezzieWithDB.ViewModels;
+
+namespace PrezzieWithDB.Controllers
+{
+    public class RequestController : Controller
+    {
+        private PrezzieContext db = new PrezzieContext();
+
+        // GET: Request
+        public ActionResult Index()
+        {
+            var requests = db.requests.Include(r => r.customer).Include(r => r.souvenir);
+            return View(requests.ToList());
+        }
+
+        // GET: Request/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(request);
+        }
+
+        // GET: Request/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Request/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(RequestView model)
+        {
+            if (ModelState.IsValid)
+            {
+                SouvenirInfo souvenirInfo = new SouvenirInfo();
+                souvenirInfo.souvenirId = model.souvenirID;
+                souvenirInfo.price = model.price;
+                souvenirInfo.descriptionSouv = model.descriptionSouv;
+
+                db.souvenirInfos.Add(souvenirInfo);
+                db.SaveChanges();
+
+                Souvenir souvenir = new Souvenir();
+                souvenir.souvenirId = souvenirInfo.souvenirId;
+                souvenir.souvenirName = model.souvenirName;
+                souvenir.countrySouv = model.countrySouv;
+
+                db.souvenirs.Add(souvenir);
+                db.SaveChanges();
+
+                Customer customer = db.customers.Find(Session["userName"]);
+
+                Request request = new Request();
+                request.souvenir = souvenir;
+                request.customer = customer;
+                request.amount = model.amount;
+                request.reward = model.reward;
+                request.status = "new";
+                request.souvenirID = model.souvenirID;
+
+                db.requests.Add(request);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // GET: Request/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userName = new SelectList(db.customers, "userName", "countryUser", request.userName);
+            ViewBag.souvenirID = new SelectList(db.souvenirs, "souvenirId", "souvenirName", request.souvenirID);
+            return View(request);
+        }
+
+        // POST: Request/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "souvenirID,userName,amount,reward,status")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(request).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.userName = new SelectList(db.customers, "userName", "countryUser", request.userName);
+            ViewBag.souvenirID = new SelectList(db.souvenirs, "souvenirId", "souvenirName", request.souvenirID);
+            return View(request);
+        }
+
+        // GET: Request/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(request);
+        }
+
+        // POST: Request/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Request request = db.requests.Find(id);
+            db.requests.Remove(request);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PrezzieWithDB/ViewModels/RequestView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrezzieWithDB.ViewModels
+{
+    public class RequestView
+    {
+        public int souvenirID { get; set; }
+        public string userName { get; set; }
+        public int amount { get; set; }
+        public string reward { get; set; }
+        public string status { get; set; }
+
+        public string souvenirName { get; set; }
+        public string countrySouv { get; set; }
+
+        public float price { get; set; }
+        public string descriptionSouv { get; set; }
+
+        public string eMail { get; set; }
+        public string firstName { get; set; }
+        public string surname { get; set; }
+        public string birthday { get; set; }
+        public string descriptionUser { get; set; }
+
+    }
+}
